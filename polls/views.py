e17300de6d7e666c9d8b3e8131d3d9af105cfaad
YunFei <mayunfei6@gmail.com>
2018-06-09 23:11:31 +0800

    tutorial 04 使用 generic.ListView generic.DetailView 简化代码，简化 url

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
@@ -29,6 +31,24 @@ def index(request):
     ##################### 2 快捷函数返回模板
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def detail(request, question_id):
@@ -66,6 +86,6 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
 
-        return HttpResponseRedirect(reversed('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
     # return HttpResponse("You're voting on question %s." % question_id)
