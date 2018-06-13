556a5ce92d732cf5ddfd8db45feabfd319f8e9ba
YunFei <mayunfei6@gmail.com>
2018-06-14 00:15:46 +0800

    路由配置 页面映射

---
+++ login/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'login'
+urlpatterns = [
+    # ex:polls/
+    # name 属性可以让你在模板 中用 {% url %} 引用避免写死
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+
+]
