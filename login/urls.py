e80b11dfea28557e7395328103326fed6477619e
YunFei <mayunfei6@gmail.com>
2018-06-14 23:04:43 +0800

    设置模板 引入 Bootstrap jQuery 写登录页面

--- login/urls.py
+++ login/urls.py
@@ -6,7 +6,7 @@ app_name = 'login'
 urlpatterns = [
     # ex:polls/
     # name 属性可以让你在模板 中用 {% url %} 引用避免写死
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
