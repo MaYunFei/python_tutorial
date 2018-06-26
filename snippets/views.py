597e7a2bc25cb2b94403c59e91f3f0aadb2625e1
YunFei <mayunfei6@gmail.com>
2018-06-26 22:36:41 +0800

    序列化 generics 优化代码

--- snippets/views.py
+++ snippets/views.py
@@ -1,40 +1,17 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
 # Create your views here.
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    # mixins.CreateModelMixin 可以保存数据
-    # generics.GenericAPIView 继承了APIView
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        # self.list是ListModelMixin的list函数
-        # 功能是过滤、分页、调用serializer,将数据序列化
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
